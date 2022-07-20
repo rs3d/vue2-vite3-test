907bdc721d8f91590fb5103f325eb982a1148dad
rs3d <rob@rs3d.de>
2022-07-20 19:26:28 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test: () => import('./components/Test.vue'),
+    Test2: Test,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+    <img src="@/assets/logo.svg" alt="Vite logo" />
+    <!--<HelloWorld msg="Hello Vue 2 + Vite 3" />-->
+    <Test msg="Hello Vue 2 + Vite 3" />
+    <Test2 msg="Hello Vue 2 + Vite 3" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { Component, Vue, Mixins } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Mixins() {
+  get default() {
+    return 1;
+  }
+  // default
+  defaultModel = {
+    test: 'x',
+
+  }
+
+  private defaults = {
+    key: 'value',
+  }
+  
+  created() {
+    this.defaultModel.test = this.defaults.key;
+  }
+}
+</script>
+
+<template>
+  <div class="example">
+    <p>
+      BUG?
+      <pre>{{ defaults }}</pre>
+      <pre>{{ defaultModel }}</pre>
+      
+      <code>components/Test.vue</code> to test hot module replacement.
+    </p>
+  <hr />  
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+  .example {
+    border: 1px solid red;
+    padding: 1rem;
+    border-radius: 1rem;
+    text-align: left;
+  }
+</style>
